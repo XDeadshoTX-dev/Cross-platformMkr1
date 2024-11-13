12d0270a585ebf293afb9d81447bc22d20557633
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-11-13 10:07:09 +0200

    Added tests

--- Project/Project.xUnitTests/UnitTest1.cs
+++ Project/Project.xUnitTests/UnitTest1.cs
@@ -1,11 +1,23 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
 namespace Project.xUnitTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
-
+        [Theory]
+        [InlineData(new string[] { "???", "abc" }, 64)]
+        [InlineData(new string[] { "???", "000" }, 1)]
+        [InlineData(new string[] { "abc", "999" }, 0)]
+        [InlineData(new string[] { "abc", "abc" }, 64)]
+        [InlineData(new string[] { "efg", "abc" }, 0)]
+        [InlineData(new string[] { "????", "efgc" }, 256)]
+        [InlineData(new string[] { "abcd", "aaaa" }, 24)]
+        public void Test(string[] inputLines, int expected)
+        { 
+            // Act
+            int actual = Program.GetResult(inputLines); 
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
